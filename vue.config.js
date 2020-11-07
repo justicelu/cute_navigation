2e037a04846769f7f3cdcb9664fa80dc76e564c5
lu <lu488383291@outlook.com>
2020-11-07 18:15:19 +0800

    第八次尝试

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    // 这里是脚手架的配置,
-    // 其中如果想修改 webpack 配置
-    publicPath: '',
+        // 这里是脚手架的配置,
+        // 其中如果想修改 webpack 配置
+        const BASE_URL = process.env.NODE_ENV === 'production' ? '/xxx(打包文件所在目录名)/' : '/'
+        module.exports = {
+            publicPath: BASE_URL,
 
-}
+        }
